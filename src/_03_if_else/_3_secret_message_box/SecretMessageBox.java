096d46c49cef63f439b988fd065b6356a41c811f
league <league@imac-22.attlocal.net>
2022-03-04 17:11:37 -0800

    new

--- src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
+++ src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
@@ -19,7 +19,7 @@ public class SecretMessageBox {
 	// 0. Make a main method and put steps 1-5 inside it
 	public static void main(String[] args) {
 		
-	String fig = "blehh";
+	String fig = "bleh";
 	// 1. Set a password in a String variable
 	
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
